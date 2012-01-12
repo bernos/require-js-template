1c9b57fe3d6cae02541d0a56bee917732946c6c4
Brendan McMahon <bernos@gmail.com>
2012-01-12 12:00:46 +1100

    Build script now builds piewpiew-backbone separately to piewpiew-core

--- build.sh
+++ build.sh
@@ -1,10 +1,27 @@
 #! /bin/bash
+
+#-----------------------------------------------------------------------------
+# piewpiew-core
+#-----------------------------------------------------------------------------
 MAJOR=0
 MINOR=0
 POINT=1
+IN=src/js/libs/piewpiew/piewpiew-core.js
+OUT=src/js/libs/piewpiew/piewpiew-core-$MAJOR.$MINOR.$POINT.min.js
+
+echo "/* piewpiew-core library v$MAJOR.$MINOR.$POINT */" > $OUT
+
+uglifyjs -nc $IN >> $OUT
+
+#-----------------------------------------------------------------------------
+# piewpiew-backbone
+#-----------------------------------------------------------------------------
+MAJOR=0
+MINOR=0
+POINT=1
+IN=src/js/libs/piewpiew/piewpiew-backbone.js
 OUT=src/js/libs/piewpiew/piewpiew-backbone-$MAJOR.$MINOR.$POINT.min.js
 
 echo "/* piewpiew-backbone library v$MAJOR.$MINOR.$POINT */" > $OUT
 
-
-uglifyjs -nc src/js/libs/piewpiew/piewpiew-backbone.js >> $OUT
+uglifyjs -nc $IN >> $OUT
