8c4c81b90ee3d784790c647f5c03688bcdc89ee7
Brendan McMahon <bernos@gmail.com>
2012-02-08 21:57:23 +1100

    Updated to latest builds off piewpiew-core and piewpiew-backbone

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,8 @@ require.config({
     'jquery':     'libs/jquery/jquery-1.7.1.min',
     'underscore': 'libs/underscore/underscore-1.2.4.min',
     'backbone':   'libs/backbone/backbone-0.5.3.min',
-    'piewpiew':   'libs/piewpiew/piewpiew-core-0.0.1.min', 
-    'piewpiew-backbone':   'libs/piewpiew/piewpiew-backbone',
+    'piewpiew':   'libs/piewpiew/piewpiew-core-0.0.2.min', 
+    'piewpiew-backbone':   'libs/piewpiew/piewpiew-backbone-0.0.2.min',
 
     // Require JS plugins
     'text':       'libs/requirejs/text',
